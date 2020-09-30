52a2a6328251f3c441d480a4029a630af77289e5
GadzhievSA <s.gadzhiev@id-mt.ru>
2020-09-30 11:45:50 +0300

    если завершено с ошибкой то suppress user exceptions true

--- src/main/java/ru/gadjini/telegram/converter/dao/ConversionQueueDao.java
+++ src/main/java/ru/gadjini/telegram/converter/dao/ConversionQueueDao.java
@@ -122,7 +122,7 @@ public class ConversionQueueDao {
 
     public void updateException(int id, int status, String exception) {
         jdbcTemplate.update(
-                "UPDATE " + TYPE + " SET exception = ?, status = ? WHERE id = ?",
+                "UPDATE " + TYPE + " SET exception = ?, status = ?, suppress_user_exceptions = true WHERE id = ?",
                 ps -> {
                     ps.setString(1, exception);
                     ps.setInt(2, status);
