1013450ffb25052a66ac5740915d82633f9b5cf1
GadzhievSA <s.gadzhiev@id-mt.ru>
2021-03-14 10:27:52 +0300

    PMD

--- src/main/java/ru/gadjini/telegram/converter/service/conversion/impl/VideoCompressConverter.java
+++ src/main/java/ru/gadjini/telegram/converter/service/conversion/impl/VideoCompressConverter.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.meta.api.objects.User;
 import ru.gadjini.telegram.converter.common.MessagesProperties;
 import ru.gadjini.telegram.converter.domain.ConversionQueueItem;
 import ru.gadjini.telegram.converter.exception.ConvertException;
