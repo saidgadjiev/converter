dd60dc2f55f9ae3a80e69500b756e1043773e25a
GadzhievSA <s.gadzhiev@id-mt.ru>
2021-07-23 10:33:29 +0300

    Убрал не нужный фильтр

--- src/main/java/ru/gadjini/telegram/converter/configuration/BotConfiguration.java
+++ src/main/java/ru/gadjini/telegram/converter/configuration/BotConfiguration.java
@@ -41,12 +41,12 @@ public class BotConfiguration {
     @Bean
     public BotFilter botFilter(UpdateFilter updateFilter, UserSynchronizedFilter userSynchronizedFilter,
                                StartCommandFilter startCommandFilter, TechWorkFilter techWorkFilter,
-                               MediaFilter mediaFilter, LastActivityFilter activityFilter,
+                               LastActivityFilter activityFilter,
                                ChannelSubscriptionFilter subscriptionFilter, UpdatesHandlerFilter updatesHandlerFilter,
                                PaidSubscriptionFilter paidSubscriptionFilter, DistributionFilter distributionFilter) {
         updateFilter.setNext(userSynchronizedFilter)
                 .setNext(startCommandFilter).setNext(subscriptionFilter).setNext(activityFilter)
-                .setNext(distributionFilter).setNext(mediaFilter)
+                .setNext(distributionFilter)
                 .setNext(techWorkFilter).setNext(paidSubscriptionFilter).setNext(updatesHandlerFilter);
 
         return updateFilter;
